da5603867d0c95927486938c17778dc427971b10
Yannis-tech <yannis.pigorsch@gmx.de>
2021-07-14 11:21:39 +0200

    About Me completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
         <Navbar />
         <Header />
+        <AboutMe />
     </div>
   );
 }
