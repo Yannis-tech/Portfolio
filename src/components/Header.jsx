ea10a2e07c3ae1e0a888d2404bf4edfecce2ae42
Yannis-tech <yannis.pigorsch@gmx.de>
2022-06-03 08:35:37 +0200

    Background optimization

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,15 +6,15 @@ const Header = () => {
             <div className="main-info">
                 <h3>Yannis Pigorsch</h3>
                 <h1>GIS Specialist / Web Developer</h1>
-            <Typed
-                className="typed-text"
-                strings={["HTML5", "CSS3", "Javascript ES6", "React", "SQL", "Git", "Geographic Information Systems", "FME"]}
-                typeSpeed={40}
-                backSpeed={60}  
-                backDelay={3000}  
-                loop          
-            />
-            <a href="#contact" className="btn-main-offer">Contact me</a>
+                <Typed
+                    className="typed-text"
+                    strings={["HTML5", "CSS3", "Javascript ES6", "React", "SQL", "Git", "Geographic Information Systems", "FME"]}
+                    typeSpeed={40}
+                    backSpeed={60}  
+                    backDelay={3000}  
+                    loop          
+                />
+                <a href="#contact" className="btn-main-offer">Contact me</a>
             </div>
         </div>
     );
